578b87f7f60f081d637d3d2bdf9b0ee78a5762fb
aaron <acyanesswe@gmail.com>
2022-12-26 23:17:15 -0800

    added new keymap for find file

--- lua/aaron/core/keymaps.lua
+++ lua/aaron/core/keymaps.lua
@@ -36,6 +36,7 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>fe", ":NvimTreeFindFile<CR>")
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
